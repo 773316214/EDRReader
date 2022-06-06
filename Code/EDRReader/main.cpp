3a484795e03d470669ac2e27c5eae2d6f04d3f83
DuanZhaobing <dzb19970702@gmail.com>
2022-06-06 19:13:05 +0800

    Set Ico
    add ico and set mainwindoe ico.

--- Code/EDRReader/main.cpp
+++ Code/EDRReader/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QApplication::setWindowIcon(QIcon(":/image/Image/EDRReader.ico"));
     w.show();
     return a.exec();
 }
